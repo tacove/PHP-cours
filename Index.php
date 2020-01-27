982c62753e4e8106acf0a7b3aa5b7215b0367489
tacove <49710990+tacove@users.noreply.github.com>
2020-01-27 11:47:26 +0100

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,16 @@
+<?php
+foreach(range('A','Z') as $lettregauche1){
+    foreach(range('A','Z') as $lettregauche2){
+        foreach(range('0','9') as $chiffre){
+            foreach(range(start'0','9') as )
+            foreach(range('A','Z') as $lettredroite1){
+                foreach(range('A','Z')as $lettredroite2){
+                    echo $lettregauche1.$lettregauche2.'-'.$chiffre.$lettredroite1.$lettredroite2."<br>";
+                }
+            }
+        }
+    }
+}
+?>
+
+
